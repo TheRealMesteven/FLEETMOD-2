5f28ff00b099f186b2495baee6de046bee29525c
Mest <mestevenemail@gmail.com>
2022-11-20 16:35:55 +0000

    Add project files.

---
+++ FLEETMOD 2/Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PulsarModLoader;
+
+namespace FLEETMOD_2
+{
+    public class Mod : PulsarMod
+    {
+        public static string myversion = "FLEETMOD V2.0.0";
+        public override string Version => Mod.myversion;
+        public override string Author => "Mest";
+        public override string Name => "FleetMod V2";
+        public override int MPFunctionality => 0;
+        public override string HarmonyIdentifier() => "Mest.Fleetmod";
+    }
+}
