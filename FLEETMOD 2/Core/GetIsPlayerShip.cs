2667fb1382ef7734a83bc709fc54dde619f04db3
Mest <mestevenemail@gmail.com>
2022-11-20 17:46:13 +0000

    Reformatted

--- FLEETMOD 2/Core/GetIsPlayerShip.cs
+++ FLEETMOD 2/Core/GetIsPlayerShip.cs
@@ -3,29 +3,24 @@ using PulsarModLoader.Utilities;
 
 namespace FLEETMOD_2.Core
 {
-	[HarmonyPatch(typeof(PLShipInfoBase), "GetIsPlayerShip")]
-	internal class GetIsPlayerShipPatch
-	{
-		public static bool Prefix(PLShipInfoBase __instance, ref bool __result)
-		{
-			if (!Global.ModEnabled)
-			{
-				return true;
-			}
-			else
-			{
-				if (Global.GetFleetShips().Contains(__instance.ShipID))
-				{
-					Logger.Info($"[FM2DB] {__instance.ShipNameValue} configured as FRIENDLY ship");
-					__result = true;
-					return false;
-				}
-				else
-				{
-					Logger.Info($"[FM2DB] {__instance.ShipNameValue} configured as HOSTILE ship");
-					return true;
-				}
-			}
-		}
-	}
+    [HarmonyPatch(typeof(PLShipInfoBase), "GetIsPlayerShip")]
+    internal class GetIsPlayerShipPatch
+    {
+        public static bool Prefix(PLShipInfoBase __instance, ref bool __result)
+        {
+            if (!Global.ModEnabled) return true;
+            if (Global.GetFleetShips().Contains(__instance.ShipID))
+            {
+                Logger.Info($"[FM2DB] {__instance.ShipNameValue} configured as FRIENDLY ship");
+                __result = true;
+                return false;
+            }
+            else
+            {
+                Logger.Info($"[FM2DB] {__instance.ShipNameValue} configured as HOSTILE ship");
+                return true;
+            }
+
+        }
+    }
 }
