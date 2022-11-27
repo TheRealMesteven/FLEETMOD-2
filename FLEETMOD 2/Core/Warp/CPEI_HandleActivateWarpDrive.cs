16840ac547dcb34f9ba996b34bf368f58aeb65c0
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-11-26 20:08:12 -0800

    Removed unused or useless code from CPEI_HandleActivateWarpDrive

--- FLEETMOD 2/Core/Warp/CPEI_HandleActivateWarpDrive.cs
+++ FLEETMOD 2/Core/Warp/CPEI_HandleActivateWarpDrive.cs
@@ -1,78 +1,46 @@
 ﻿using HarmonyLib;
+using PulsarModLoader.Utilities;
 
 namespace FLEETMOD_2.Core.Warp
 {
-	[HarmonyPatch(typeof(PLServer), "CPEI_HandleActivateWarpDrive")]
-	internal class CPEI_HandleActivateWarpDrive
-	{
-		public static bool Prefix(int shipID, int playerID)
-		{
-			if (!Global.ModEnabled)
-			{
-				return true;
-			}
-			else
-			{
-				// Disallow Non-Modded warping.
-				if (!Global.FleetModClients.Contains(playerID))
-				{
-					return false;
-				}
-				else
-				{
-					PLServer.Instance.photonView.RPC("AddNotification", PhotonTargets.All, new object[]
-					{
-						PLServer.Instance.GetPlayerFromPlayerID(playerID).GetPlayerName(false) + " has engaged the warp! Heading to: " + PLEncounterManager.Instance.GetShipFromID(shipID).WarpTargetID.ToString(),
-						playerID,
-						PLServer.Instance.GetEstimatedServerMs() + 3000,
-						true
-					});
+    [HarmonyPatch(typeof(PLServer), "CPEI_HandleActivateWarpDrive")]
+    internal class CPEI_HandleActivateWarpDrive
+    {
+        public static bool Prefix(int shipID, int playerID)
+        {
+            if (!Global.ModEnabled)
+            {
+                return true;
+            }
 
-					// Strange Mikey code which permits ships to warp without going invisible.
-					int num = UnityEngine.Random.Range(0, 10);
-					if (num == 50 && PLNetworkManager.Instance.LocalPlayer != null)
-					{
-						int num2 = -1;
-						for (int i = 0; i < PLGlobal.Instance.Galaxy.AllSectorInfos.Count * 8; i++)
-						{
-							int num3 = UnityEngine.Random.Range(0, PLGlobal.Instance.Galaxy.AllSectorInfos.Count * 2);
-							if (PLGlobal.Instance.Galaxy.AllSectorInfos.ContainsKey(num3))
-							{
-								PLSectorInfo plsectorInfo = PLGlobal.Instance.Galaxy.AllSectorInfos[num3];
-								if (plsectorInfo != null && plsectorInfo.ID != PLServer.Instance.GetCurrentHubID())
-								{
-									num2 = num3;
-									break;
-								}
-							}
-						}
-						if (num2 != -1)
-						{
-							PLServer.Instance.photonView.RPC("NetworkBeginWarp", PhotonTargets.All, new object[]
-							{
-								PLEncounterManager.Instance.PlayerShip.ShipID,
-								num2,
-								PLServer.Instance.GetEstimatedServerMs(),
-								-1
-							});
-						}
-					}
-					else
-					{
-						if (PLNetworkManager.Instance.LocalPlayer != null)
-						{
-							PLServer.Instance.photonView.RPC("NetworkBeginWarp", PhotonTargets.All, new object[]
-							{
-								PLEncounterManager.Instance.PlayerShip.ShipID,
-								PLEncounterManager.Instance.GetShipFromID(shipID).WarpTargetID,
-								PLServer.Instance.GetEstimatedServerMs(),
-								-1
-							});
-						}
-					}
-					return false;
-				}
-			}
-		}
-	}
+            //Stop if warping client doesn't have FM. 
+            if (!Global.FleetModClients.Contains(playerID))
+            {
+                return false;
+            }
+
+            //Warp notification
+            PLServer.Instance.photonView.RPC("AddNotification", PhotonTargets.All, new object[]
+            {
+                PLServer.Instance.GetPlayerFromPlayerID(playerID).GetPlayerName(false) + " has engaged the warp! Heading to: " + PLEncounterManager.Instance.GetShipFromID(shipID).WarpTargetID.ToString(),
+                playerID,
+                PLServer.Instance.GetEstimatedServerMs() + 3000,
+                true
+            });
+
+
+            //Warp the ship.  -   Don't know why this localPlayer null check is here, might be able to remove it.
+            if (PLNetworkManager.Instance.LocalPlayer != null)
+            {
+                PLServer.Instance.photonView.RPC("NetworkBeginWarp", PhotonTargets.All, new object[]
+                {
+                    PLEncounterManager.Instance.PlayerShip.ShipID,
+                    PLEncounterManager.Instance.GetShipFromID(shipID).WarpTargetID,
+                    PLServer.Instance.GetEstimatedServerMs(),
+                    -1
+                });
+            }
+            return false;
+        }
+    }
 }
