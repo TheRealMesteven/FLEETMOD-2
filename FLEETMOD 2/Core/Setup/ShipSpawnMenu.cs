b80b337cc845fc3db9eaf4acb7de2a4b97da67e9
Mest <mestevenemail@gmail.com>
2022-11-28 15:51:59 +0000

    Added F6 Ship Spawning Menu

---
+++ FLEETMOD 2/Core/Setup/ShipSpawnMenu.cs
@@ -0,0 +1,101 @@
+﻿using HarmonyLib;
+using PulsarModLoader;
+using PulsarModLoader.Patches;
+using PulsarModLoader.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+
+namespace FLEETMOD_2.Core.Setup
+{
+    [HarmonyPatch(typeof(PLServer), "Update")]
+    internal class ShipSpawnMenuPatch
+    {
+        // Detect the Keybind to create a ship and then generate menu
+        public static void Postfix(PLServer __instance)
+        {
+            if (!Global.ModEnabled || __instance == null || !__instance.GameHasStarted || PLNetworkManager.Instance.LocalPlayer == null || !PLNetworkManager.Instance.LocalPlayer.GetHasStarted()
+            || PLEncounterManager.Instance.PlayerShip == null || (PLNetworkManager.Instance.LocalPlayer.GetClassID() == 0 && !PhotonNetwork.isMasterClient) || PLNetworkManager.Instance.IsTyping || !Input.GetKeyDown(KeyCode.F6))
+                return;
+            PLMusic.PostEvent("play_sx_playermenu_click_major", PLServer.Instance.gameObject);
+            PLNetworkManager.Instance.MainMenu.CloseActiveMenu();
+            PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLCreateGameMenu(true));
+            PLTabMenu.Instance.TabMenuActive = false;
+        }
+    }
+    [HarmonyPatch(typeof(PLUICreateGameMenu), "ClickEngage")]
+    internal class ShipSpawnMenu
+    {
+        // Overriding the ClickEngage button on the CreateGameMenu so that ingame it creates a ship using the information provided.
+        protected static FieldInfo FieldInfo = AccessTools.Field(typeof(PLUICreateGameMenu), "CampaignMode");
+        public static bool Prefix(PLUICreateGameMenu __instance, ref int ___CurrentSelectedShipIndex)
+        {
+            if (!Global.ModEnabled || PLServer.Instance == null || !PLServer.Instance.GameHasStarted) return true;
+            PLMusic.PostEvent("play_sx_playermenu_click_major", PLGlobal.Instance.gameObject);
+
+            // GetCurrentShipTypeID (Used by default code)
+            PLNetworkManager.Instance.SelectedShipTypeID = ___CurrentSelectedShipIndex + 1;
+            if (___CurrentSelectedShipIndex > 8)
+                PLNetworkManager.Instance.SelectedShipTypeID = ___CurrentSelectedShipIndex;
+
+            PLNetworkManager.Instance.MainMenu.CloseActiveMenu();
+
+            // Class Change & Ship Spawning occurs in this ModMessage
+            ModMessage.SendRPC(Mod.HarmonyIdent, "FLEETMOD_2.ModMessages.ServerCreateShip", PhotonTargets.MasterClient, new object[]
+            {
+                    PLNetworkManager.Instance.SelectedShipTypeID,
+                    PLNetworkManager.Instance.LocalPlayer.GetPlayerID(),
+                    __instance.ShipNameField.text
+            });
+
+            // Allow Admiral to spawn ships with Menu without becoming new Captain
+            if (!PhotonNetwork.isMasterClient)
+            {
+                PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLErrorMessageMenu(string.Concat(new string[]
+                {
+                    "<color=#0066FF>You Are Now The Captain Of ",
+                    __instance.ShipNameField.text,
+                    "</color>\n\n<color=#c0c0c0>",
+                    PLGlobal.Instance.ClassDesc[0],
+                    "</color>"
+                })));
+            }
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(PLUIInsideWorldUI), "Update")]
+    internal class UIInsideWorldUIPatch
+    {
+        // Update has an exception when called in-game due to missing a null check for PLAbyssShipInfo.Instance.Map
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(PLAbyssShipInfo), "Instance")),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(PLAbyssShipInfo), "Map")),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(PLAbyssMap), "ShowOnScreen")),
+            };
+            // Gets CodeInstruction to exit if when null == true
+            int InstructionIndex = FindSequence(instructions, targetSequence, CheckMode.ALWAYS, true) - (targetSequence.Count() + 1);
+            List<CodeInstruction> patchSequence = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UIInsideWorldUIFix), "NullCheck", null, null)),
+                instructions.ToList()[InstructionIndex]
+            };
+            return PatchBySequence(instructions, targetSequence, patchSequence, PatchMode.BEFORE, CheckMode.ALWAYS, true);
+        }
+    }
+    internal class UIInsideWorldUIFix
+    {
+        public static bool NullCheck()
+        {
+            return PLAbyssShipInfo.Instance.Map == null;
+        }
+    }
+}
---
+++ FLEETMOD 2/ModMessages/ServerCreateShip.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using PulsarModLoader;
+using PulsarModLoader.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace FLEETMOD_2.ModMessages
+{
+    internal class ServerCreateShip : ModMessage
+    {
+        public override void HandleRPC(object[] arguments, PhotonMessageInfo sender)
+        {
+            if (!Global.ModEnabled || !PhotonNetwork.isMasterClient) return;
+
+            // Spawn Ship from Prefabs
+            PLPlayer playerFromPlayerID = PLServer.Instance.GetPlayerFromPlayerID((int)arguments[1]);
+            GameObject gameObject = PhotonNetwork.Instantiate("NetworkPrefabs/" + PLGlobal.Instance.PlayerShipNetworkPrefabNames[(int)arguments[0]], new Vector3(50f, 50f, 50f), Quaternion.identity, 0, null);
+            int ShipID = PLServer.ServerSpaceTargetIDCounter++;
+            gameObject.GetComponent<PLShipInfo>().SetShipID(ShipID);
+            ShipInfo shipInfo = new ShipInfo(gameObject.GetComponent<PLShipInfo>().ShipID, new List<int>());
+            Global.FleetShips.Add(shipInfo);
+            gameObject.GetComponent<PLShipInfo>().AutoTarget = false;
+            gameObject.GetComponent<PLShipInfo>().TeamID = 1;
+            gameObject.GetComponent<PLShipInfo>().OnIsNewStartingShip();
+            gameObject.GetComponent<PLShipInfo>().ShipNameValue = (string)arguments[2];
+            gameObject.GetComponent<PLShipInfo>().LastAIAutoYellowAlertSetupTime = Time.time;
+            gameObject.GetComponent<PLShipInfo>().SetupShipStats(false, true);
+
+            // Allow Admiral to spawn ships with Menu without becoming new Captain
+            if (playerFromPlayerID != PLNetworkManager.Instance.LocalPlayer)
+            {
+                ModMessage.SendRPC(Mod.HarmonyIdent, "FLEETMOD_2.ModMessages.SetPlayerAsShip", PhotonTargets.MasterClient, new object[]
+                {
+                    PLNetworkManager.Instance.LocalPlayer.GetPlayerID(),
+                    ShipID,
+                    0
+                });
+            }
+            else
+            {
+                // FleetShipSync occurs in SetPlayerAsShip so no need to call twice
+                ModMessage.SendRPC(Mod.HarmonyIdent, "FLEETMOD_2.ModMessages.FleetShipSync", PhotonTargets.Others, Global.SerializeFleetShips(Global.FleetShips).Cast<object>().ToArray());
+            }
+            PLServer.Instance.photonView.RPC("AddCrewWarning", PhotonTargets.All, new object[]
+            {
+                "The " + (string)arguments[2] + " Has Joined!",
+                Color.green,
+                0,
+                "SHIP"
+            });
+        }
+    }
+}
+
